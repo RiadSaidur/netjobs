cc941fff19f032cf84637bb2bb25017c8d7838f5
Saidur Rahman Riad <reidvai1998@gmail.com>
2021-03-27 23:11:57 +0600

    refactor: move home global css to home.css

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+
 import Welcome from '../components/home/welcome/Welcome'
 import Promotions from '../components/home/Promotions'
-import Special from '../components/special/Special'
+import Special from '../components/home/special/Special'
+
+import '../css/home.css'
 
 const Home = () => {
   return (
-    <div className="container">
+    <div className="container home">
       <Welcome />
       <Promotions />
       <Special />
